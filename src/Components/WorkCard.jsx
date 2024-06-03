9bee471fd4ffcf4dff0113a0673f3b073785f759
Ahmed Hussin <ahmed208087@gmail.com>
2024-06-03 15:37:51 +0300

    work section in the home page done with links to the projects in github

--- src/Components/WorkCard.jsx
+++ src/Components/WorkCard.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import { FaLink } from "react-icons/fa";
 
-function WorkCard({icon,projectImg,title, description,skills }) {
+
+function WorkCard({icon,projectImg,title, description,skills,projectLink }) {
   return (
-    <div className="grid grid-cols-3 gap-1 mt-10">
+    <div className="grid grid-cols-3 gap-1 mt-1">
+      {/* right column */}
       <div className="col-span-1 flex items-center border-2 border-lines-color rounded pl-5">
-        <div className="flex  flex-col gap-5 ">
+        <div className="flex  flex-col gap-3 ">
           <div className="flex gap-2 items-center">
             <Image src={icon} alt="icon" width={30} height={30} />
             <h1 className="text-2xl font-semibold uppercase">{title}</h1>
           </div>
-          <p>{description}</p>
-          <div className="flex gap-2 ">
+          <p className="text-[#777b84]">{description}</p>
+          <div className="flex gap-2 items-center">
+            <p className='hover:underline'>
+              <Link href={{projectLink}}>
+                Check The Project On Github Now!
+              </Link>
+            </p>
+            <FaLink />
+          </div>
+          <div className="flex gap-2 mt-2 ">
             {skills.map((skill, index) => (
-              <span className="bg-[#212225] px-3 font-mono" key={index}>
+              <span
+                className="bg-[#212225] px-3 font-mono text-[#edeef0]"
+                key={index}
+              >
                 {skill}
               </span>
             ))}
           </div>
         </div>
       </div>
+      {/* image column */}
       <div className="col-span-2 border-2 border-lines-color rounded flex justify-center  p-5">
         <Image
           src={projectImg}
--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,60 @@ import Navbar from "@/Components/Navbar";
 import Hero from "@/Components/Hero";
 import Image from "next/image";
 import WorkCard from "@/Components/WorkCard";
-import reactIcon from '../../public/images.png'
+import reactIcon from "../../public/images.png";
 import projectImg from "../../public/original-accb2ef29d68a2a29119c5e3ff116f33.png";
+import { GoCodeSquare } from "react-icons/go";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto bg-background-color">
       {/* hero section */}
       <Hero />
-      {/* work section*/}
-      <div>
-
+      {/* work section title*/}
+      <div className="grid grid-cols-3 border-2 border-lines-color rounded mt-10">
+        <div className="col-span-1 border-r-2 border-lines-color rounded  p-5 flex flex-col gap-5">
+          <div className="bg-[#212225] p-5 rounded-full w-fit">
+            <GoCodeSquare />
+          </div>
+          <h1 className="text-[48px] font-semibold">
+            Work.
+            <br />{" "}
+            <span className="text-[#777b84]">
+              A selection of recent projects.
+            </span>
+          </h1>
+        </div>
+        <div className="col-span-1 border-r-2 border-lines-color rounded" />
+        <div className="col-span-1" />
       </div>
-     <WorkCard title={'React Project'} description={'This is the descriptoin of this project done by me'} icon={reactIcon} projectImg={projectImg} skills={['react','js','html']} />
+      {/* work section cards */}
+      <WorkCard
+        title={"React Project"}
+        description={"This is the descriptoin of this project done by me"}
+        icon={reactIcon}
+        projectImg={projectImg}
+        skills={["react", "js", "html"]}
+      />
+      <WorkCard
+        title={"React Project"}
+        description={"This is the descriptoin of this project done by me"}
+        icon={reactIcon}
+        projectImg={projectImg}
+        skills={["react", "js", "html"]}
+      />
+      <WorkCard
+        title={"React Project"}
+        description={"This is the descriptoin of this project done by me"}
+        icon={reactIcon}
+        projectImg={projectImg}
+        skills={["react", "js", "html"]}
+      />
+      {/* about section */}
+      <div>
+        <div>
+          <h1>Learning <br/> Developing <br/> &Innovating.</h1>
+        </div>
+      </div>
     </div>
   );
 }
