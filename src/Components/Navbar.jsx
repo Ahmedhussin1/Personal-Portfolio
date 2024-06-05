eaeaeb66df35fcdbca01b008446fad8cd8689912
Ahmed Hussin <ahmed208087@gmail.com>
2024-06-05 18:49:36 +0300

    navbar sticky effect added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,29 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { useState, useEffect } from 'react';
 function Navbar() {
+  const [borderColor, setBorderColor] = useState("#212225");
+  useEffect(()=>{
+    const handelScroll = () =>{
+      if(window.scrollY > 50 ){
+        setBorderColor("#777b84");
+      }else{
+        setBorderColor("#212225");
+      }
+    };
+    window.addEventListener('scroll', handelScroll);
+    return () => {
+      window.removeEventListener('scroll', handelScroll);
+    }
+  },[])
   return (
-    <div className='pt-2 '>
-      <div className="max-w-lg mx-auto mt-5">
-        <ul className="flex gap-5 justify-center border-2 border-[#212224] rounded-lg py-3 px-4">
+    <div className="pt-2 sticky top-0 z-10">
+      <div className="max-w-lg mx-auto mt-5 bg-background-color rounded">
+        <ul
+          className={`flex gap-5 justify-center border-2 rounded-lg py-3 px-4`}
+          style={{ borderColor }}
+        >
           <li>
             <Link href="/">Home</Link>
           </li>
