b25431ae920bf11dc5d150ecd23f98ad8b9b7fbc
Ahmed Hussin <ahmed208087@gmail.com>
2024-06-05 16:24:23 +0300

    routing fixed to the right data based on the id. single project page done

--- src/app/work/[projectId]/page.jsx
+++ src/app/work/[projectId]/page.jsx
@@ -2,7 +2,10 @@
 import { useRouter } from "next/navigation";
 import Image from 'next/image'
 import React, { useEffect, useState } from "react";
-import singleProjectData from "../../../data/singleWorkCardData";
+import singleProjectData from "../../../data/projectData";
+import GetInTouch from "@/Components/GetInTouch";
+import { FaGithub } from "react-icons/fa6";
+
 function SingleProjectPage({params}) {
   const router = useRouter();
 //   const [projectData, setProjectData] = useState(null)
@@ -13,21 +16,100 @@ function SingleProjectPage({params}) {
   if (!projectData) {
     return <h1>404 Not Found</h1>;
   }
+  const formattedFeature = projectData.features.split('. ').join('\n');
 
+  return (
+    <div className="max-w-7xl mx-auto mt-10 flex flex-col  justify-between ">
+      {/* title */}
+      <div className="flex w-full justify-between items-center py-20">
+        <div className="flex flex-col justify-center max-w-xl ">
+          <h1 className="text-[32px] font-semibold">{projectData.title}</h1>
+          <span className="text-[32px] font-semibold text-[#777b84]">
+            {projectData.shortDesc}
+          </span>
+        </div>
+        {/* tech used */}
+        <div className="grid grid-cols-3 gap-2">
+          {projectData.techUsed.map((tech, index) => (
+            <span
+              key={index}
+              className="bg-[#212225] px-3 font-mono text-[#edeef0] text-[14px] rounded text-center"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
 
-  return <div>
-    <div>
-        <h1>{projectData.title}</h1>
+      {/* main image */}
+      <div className="flex justify-center items-center ">
+        <Image
+          src={projectData.MainImage}
+          alt="project image"
+          width={1000}
+          height={800}
+          className="rounded"
+        />
+      </div>
+      {/* features */}
+      <div className="pt-20">
+        <h1 className="text-[32px]">Features</h1>
+        {projectData.features.split(". ").map((feature, index) => (
+          <p className="leading-8 text-[#777b84]" key={index}>
+            {feature}.
+          </p>
+        ))}
+      </div>
+      {/* mobile Image */}
+      <div className="flex justify-center items-center pt-20">
+        <Image
+          src={projectData.mobileImage}
+          alt="mobile image"
+          width={800}
+          height={800}
+          className="rounded"
+        />
+      </div>
+      {/* objectives section */}
+      <div className="pt-20">
+        <h1 className="text-[32px]">Objectives</h1>
+        <p className="leading-8 text-[#777b84]">{projectData.objective}</p>
+      </div>
+      {/* responsive design image */}
+      <div className="grid grid-cols-2 pt-20">
+        <div className="col-span-1 flex justify-center items-center">
+          <Image
+            src={projectData.image2}
+            alt="mobile view image"
+            className="rounded"
+            width={300}
+            height={600}
+          />
+        </div>
+        <div className="col-span-1 flex justify-center items-center">
+          <Image
+            src={projectData.image2}
+            alt="mobile view image"
+            className="rounded"
+            width={300}
+            height={600}
+          />
+        </div>
+      </div>
+      {/* links */}
+      <div className="py-20 border-b-2 border-lines-color flex flex-col gap-5">
+        <h1 className="text-[32px]">Links</h1>
+        <a
+          className="hover:underline text-[24px] flex items-center gap-2 cursor-pointer"
+          href={projectData.githubLink}
+        >
+          <FaGithub />
+          Github
+        </a>
+      </div>
+      <GetInTouch />
     </div>
-    <div>
-        <p>
-            {projectData.description}
-        </p>
-    </div>
-    <div>
-        <Image src={projectData.projectImg} alt="project image" width={300} height={300} />
-    </div>
-  </div>;
+  );
 }
 
 export default SingleProjectPage;
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,6 +1,6 @@
 const singleProjectData = [
   {
-    id: "1",
+    id: 1,
     title: "AI summarizer",
     description:
       "Summarize any kind of article with just one click using the powerful OpenAI model.If you're getting started and need assistance or face any bugs, join our active Discord community with over 27k+ members. It's a place where people help each other out.",
@@ -17,4 +17,96 @@ const singleProjectData = [
       "👉 Modern User Interface: A modern and user-friendly interface, offering an intuitive experience for users.👉 Summary Generation: Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content.👉 History Saving with Local Storage: The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history.👉 Copy to Clipboard Functionality: Enables users to easily share or store the summarized content by copying it to their clipboard.👉 Advanced RTK Query API Requests: Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management.and many more, including code architecture and reusability",
     github: "https://github.com/Ahmedhussin1/Ai-summarizer",
   },
+  {
+    id: 2,
+    title: "AI summarizer 2",
+    shortDesc: "summarize any article just with one click",
+    description:
+      "Summarize any kind of article with just one click using the powerful OpenAI model.If you're getting started and need assistance or face any bugs, join our active Discord community with over 27k+ members. It's a place where people help each other out.",
+    MainImage: "/projectsIcons/AorJtjCQpCBcZQotur7j.avif",
+    mobileImage: "/projectsIcons/kenusbQCSrmC50LEycB0.avif",
+    image1: "/projectsIcons/eMeNpJlyTki7vJloBLIW.avif",
+    image2: "/projectsIcons/image.png",
+    image3: "/projectsIcons/image.png",
+    image4: "",
+    objective:
+      "Publisher Prime embarked on a pioneering project to revolutionize the way helicopter pilots are trained. Our mission was to harness our platform's potential to blend technology with storytelling, thereby creating a deeply engaging and educational interactive eBook. This eBook was designed to simulate various flight principles, incorporating videos, texts, and game-like controls with instructions, enabling students to immerse themselves fully in their learning experience.",
+    techUsed: ["React.js", "TypeScript", "Redux Toolkit", "Tailwind CSS"],
+    features:
+      " Modern User Interface: A modern and user-friendly interface, offering an intuitive experience for users. Summary Generation: Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content. History Saving with Local Storage: The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history. Copy to Clipboard Functionality: Enables users to easily share or store the summarized content by copying it to their clipboard. Advanced RTK Query API Requests: Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management.and many more, including code architecture and reusability",
+    github: "https://github.com/Ahmedhussin1/Ai-summarizer",
+  },
+  {
+    id: 3,
+    title: "AI summarizer 3",
+    description:
+      "Summarize any kind of article with just one click using the powerful OpenAI model.If you're getting started and need assistance or face any bugs, join our active Discord community with over 27k+ members. It's a place where people help each other out.",
+    MainImage: "/projectsIcons/AorJtjCQpCBcZQotur7j.avif",
+    mobileImage: "/projectsIcons/kenusbQCSrmC50LEycB0.avif",
+    image1: "/projectsIcons/eMeNpJlyTki7vJloBLIW.avif",
+    image2: "",
+    image3: "",
+    image4: "",
+    objective:
+      "Publisher Prime embarked on a pioneering project to revolutionize the way helicopter pilots are trained. Our mission was to harness our platform's potential to blend technology with storytelling, thereby creating a deeply engaging and educational interactive eBook. This eBook was designed to simulate various flight principles, incorporating videos, texts, and game-like controls with instructions, enabling students to immerse themselves fully in their learning experience.",
+    techUsed: ["React.js", "TypeScript", "Redux Toolkit", "Tailwind CSS"],
+    features:
+      "👉 Modern User Interface: A modern and user-friendly interface, offering an intuitive experience for users.👉 Summary Generation: Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content.👉 History Saving with Local Storage: The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history.👉 Copy to Clipboard Functionality: Enables users to easily share or store the summarized content by copying it to their clipboard.👉 Advanced RTK Query API Requests: Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management.and many more, including code architecture and reusability",
+    github: "https://github.com/Ahmedhussin1/Ai-summarizer",
+  },
+  {
+    id: 4,
+    title: "AI summarizer 4",
+    description:
+      "Summarize any kind of article with just one click using the powerful OpenAI model.If you're getting started and need assistance or face any bugs, join our active Discord community with over 27k+ members. It's a place where people help each other out.",
+    MainImage: "/projectsIcons/AorJtjCQpCBcZQotur7j.avif",
+    mobileImage: "/projectsIcons/kenusbQCSrmC50LEycB0.avif",
+    image1: "/projectsIcons/eMeNpJlyTki7vJloBLIW.avif",
+    image2: "",
+    image3: "",
+    image4: "",
+    objective:
+      "Publisher Prime embarked on a pioneering project to revolutionize the way helicopter pilots are trained. Our mission was to harness our platform's potential to blend technology with storytelling, thereby creating a deeply engaging and educational interactive eBook. This eBook was designed to simulate various flight principles, incorporating videos, texts, and game-like controls with instructions, enabling students to immerse themselves fully in their learning experience.",
+    techUsed: ["React.js", "TypeScript", "Redux Toolkit", "Tailwind CSS"],
+    features:
+      "👉 Modern User Interface: A modern and user-friendly interface, offering an intuitive experience for users.👉 Summary Generation: Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content.👉 History Saving with Local Storage: The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history.👉 Copy to Clipboard Functionality: Enables users to easily share or store the summarized content by copying it to their clipboard.👉 Advanced RTK Query API Requests: Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management.and many more, including code architecture and reusability",
+    github: "https://github.com/Ahmedhussin1/Ai-summarizer",
+  },
+  {
+    id: 5,
+    title: "AI summarizer 5",
+    description:
+      "Summarize any kind of article with just one click using the powerful OpenAI model.If you're getting started and need assistance or face any bugs, join our active Discord community with over 27k+ members. It's a place where people help each other out.",
+    MainImage: "/projectsIcons/AorJtjCQpCBcZQotur7j.avif",
+    mobileImage: "/projectsIcons/kenusbQCSrmC50LEycB0.avif",
+    image1: "/projectsIcons/eMeNpJlyTki7vJloBLIW.avif",
+    image2: "",
+    image3: "",
+    image4: "",
+    objective:
+      "Publisher Prime embarked on a pioneering project to revolutionize the way helicopter pilots are trained. Our mission was to harness our platform's potential to blend technology with storytelling, thereby creating a deeply engaging and educational interactive eBook. This eBook was designed to simulate various flight principles, incorporating videos, texts, and game-like controls with instructions, enabling students to immerse themselves fully in their learning experience.",
+    techUsed: ["React.js", "TypeScript", "Redux Toolkit", "Tailwind CSS"],
+    features:
+      "👉 Modern User Interface: A modern and user-friendly interface, offering an intuitive experience for users.👉 Summary Generation: Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content.👉 History Saving with Local Storage: The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history.👉 Copy to Clipboard Functionality: Enables users to easily share or store the summarized content by copying it to their clipboard.👉 Advanced RTK Query API Requests: Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management.and many more, including code architecture and reusability",
+    github: "https://github.com/Ahmedhussin1/Ai-summarizer",
+  },
+  {
+    id: 6,
+    title: "AI summarizer 6",
+    description:
+      "Summarize any kind of article with just one click using the powerful OpenAI model.If you're getting started and need assistance or face any bugs, join our active Discord community with over 27k+ members. It's a place where people help each other out.",
+    MainImage: "/projectsIcons/AorJtjCQpCBcZQotur7j.avif",
+    mobileImage: "/projectsIcons/kenusbQCSrmC50LEycB0.avif",
+    image1: "/projectsIcons/eMeNpJlyTki7vJloBLIW.avif",
+    image2: "",
+    image3: "",
+    image4: "",
+    objective:
+      "Publisher Prime embarked on a pioneering project to revolutionize the way helicopter pilots are trained. Our mission was to harness our platform's potential to blend technology with storytelling, thereby creating a deeply engaging and educational interactive eBook. This eBook was designed to simulate various flight principles, incorporating videos, texts, and game-like controls with instructions, enabling students to immerse themselves fully in their learning experience.",
+    techUsed: ["React.js", "TypeScript", "Redux Toolkit", "Tailwind CSS"],
+    features:
+      "👉 Modern User Interface: A modern and user-friendly interface, offering an intuitive experience for users.👉 Summary Generation: Users can input the URL of a lengthy article, and the web app utilizes AI to provide a concise summary of the article content.👉 History Saving with Local Storage: The app includes a history feature, allowing users to save summaries locally, providing a convenient way to revisit and manage their reading history.👉 Copy to Clipboard Functionality: Enables users to easily share or store the summarized content by copying it to their clipboard.👉 Advanced RTK Query API Requests: Utilizes the advanced capabilities of Redux Toolkit (RTK) Query for making API requests. These requests fire conditionally based on specific criteria, optimizing data fetching and management.and many more, including code architecture and reusability",
+    github: "https://github.com/Ahmedhussin1/Ai-summarizer",
+  },
 ];
+export default singleProjectData;
